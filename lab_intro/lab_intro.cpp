7a1a36c42fcce6b679b03a4f57bcadcda08593f3
Chirag Dev Bhatia <cbhatia3@linux-ssh-06.ews.illinois.edu>
2021-09-01 19:09:53 -0500

    finished lab

--- lab_intro/lab_intro.cpp
+++ lab_intro/lab_intro.cpp
@@ -61,9 +61,22 @@ PNG grayscale(PNG image) {
  * @return The image with a spotlight.
  */
 PNG createSpotlight(PNG image, int centerX, int centerY) {
+  for (unsigned int x = 0; x < image.width(); x++) {
+    for (unsigned int y = 0; y < image.height(); y++) {
+      HSLAPixel &pixel = image.getPixel(x, y);
+      int x_distance = centerX - x;
+      int y_distance = centerY - y;
+      double distance_from_center = sqrt((x_distance * x_distance) + (y_distance * y_distance));
+      if (distance_from_center < 160) {
+        double decrease_amount = 0.005 * distance_from_center;
 
+        pixel.l = (pixel.l * (1 - decrease_amount));
+      } else {
+        pixel.l = (pixel.l * 0.2);
+      }
+    }
+  }
   return image;
-  
 }
  
 
@@ -77,8 +90,21 @@ PNG createSpotlight(PNG image, int centerX, int centerY) {
  *
  * @return The illinify'd image.
 **/
-PNG illinify(PNG image) {
 
+// illini orange = 11 and blue = 216
+PNG illinify(PNG image) {
+  for (unsigned int x = 0; x < image.width(); x++) {
+    for (unsigned int y = 0; y < image.height(); y++) {
+      HSLAPixel &pixel = image.getPixel(x, y);
+      double min_boundary_blue = ((216 - 11)/2) + 11;
+      double max_boundary_blue = ((341 - 216)/2) + 216;
+      if (pixel.h >= min_boundary_blue && pixel.h < max_boundary_blue) {
+        pixel.h = 216;
+      } else {
+        pixel.h = 11;
+      }
+    }
+  }
   return image;
 }
  
@@ -96,6 +122,26 @@ PNG illinify(PNG image) {
 * @return The watermarked image.
 */
 PNG watermark(PNG firstImage, PNG secondImage) {
-
+  unsigned largest_width = 0;
+  unsigned largest_height = 0;
+  if (firstImage.width() >= secondImage.width()) {
+    largest_width = firstImage.width();
+  } else {
+    largest_width = secondImage.width();
+  }
+  if (firstImage.height() >= secondImage.height()) {
+    largest_height = firstImage.height();
+  } else {
+    largest_height = secondImage.height();
+  }
+  for (unsigned int x = 0; x < largest_width; x++) {
+    for (unsigned int y = 0; y < largest_height; y++) {
+      HSLAPixel &pixel_one = firstImage.getPixel(x, y);
+      HSLAPixel &pixel_two = secondImage.getPixel(x, y);
+      if (pixel_two.l == 1.0) {
+        pixel_one.l = fmin(pixel_one.l + 0.2, 1.0);
+      }
+    }
+  }
   return firstImage;
 }
--- lab_intro/lab_intro.h
+++ lab_intro/lab_intro.h
@@ -7,6 +7,7 @@
 
 #include "cs225/PNG.h"
 
+
 cs225::PNG grayscale(cs225::PNG image);  
 cs225::PNG createSpotlight(cs225::PNG image, int centerX, int centerY);
 cs225::PNG illinify(cs225::PNG image);
