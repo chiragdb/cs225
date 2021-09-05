4bd132fafcfaa01ef9f13a456830383a95f4282e
Chirag Dev Bhatia <cbhatia3@linux-ssh-10.ews.illinois.edu>
2021-09-05 14:26:59 -0500

    finished mp

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -5,12 +5,31 @@
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  cs225::PNG input;
+  input.readFromFile(inputFile);
+  unsigned int w = input.width();
+  unsigned int h = input.height();
+  cs225::PNG output(w, h);
+  for (unsigned int x = 0; x < w; x++) {
+    for (unsigned int y = 0; y < h; y++) {
+      output.getPixel(x, y) = input.getPixel(w-x-1, h-y-1);
+    }
+  }
+  output.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
-  // TODO: Part 3
+  for (unsigned int x = 0; x < width; x++) {
+    for (unsigned int y = 0; y < height; y++) {
+      cs225::HSLAPixel pix = png.getPixel(x,y);
+      pix.a = 1.0;
+      // pix.h = ((2.1 * x) + 44) + ((1.7 * y) + 67);
+      pix.h = 216;
+      pix.l = 3.7 * y;
+      pix.s = 0.8 * (x + y);
+    }
+  }
 
   return png;
 }
